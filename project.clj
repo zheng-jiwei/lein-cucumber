62c1fd6caae8c162c2bf0c52b18aff00bc0d8164
Verneri Åberg <verneri.aberg@gmail.com>
2015-10-28 12:50:47 +0200

    Bump version to 1.0.7-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.siili/lein-cucumber "1.0.6"
+(defproject com.siili/lein-cucumber "1.0.7-SNAPSHOT"
   :description "Run cucumber-jvm specifications with leiningen"
   :url https://github.com/siilisolutions/lein-cucumber
   :dependencies [[info.cukes/cucumber-clojure "1.2.4"]
