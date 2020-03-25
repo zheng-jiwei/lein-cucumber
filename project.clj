d3093646d14664f806fa4f5a0347f59714ebd176
zheng-jiwei <zheng.jiwei@connecty.co.jp>
2020-03-25 17:44:55 +0900

    update version to 1.0.8

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject com.siili/lein-cucumber "1.0.7"
+(defproject com.siili/lein-cucumber "1.0.8"
   :description "Run cucumber-jvm specifications with leiningen"
   :url https://github.com/siilisolutions/lein-cucumber
   :dependencies [[info.cukes/cucumber-clojure "1.2.6"]
                  [leiningen-core "2.0.0"]
                  [org.clojure/clojure "1.8.0"]]
   :profiles {:cucumber {:dependencies [[commons-io "2.4"]]
-                        :plugins [[com.siili/lein-cucumber "1.0.7"]]}
+                        :plugins [[com.siili/lein-cucumber "1.0.8"]]}
              :cuke-htmlrep [:cucumber {:cucumber
                                        {:formatter {:type :html
                                                     :path "target/report"}}}]}
