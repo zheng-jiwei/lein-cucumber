f201a0710dfe873383d9e9320e0c61077cda9784
zheng-jiwei <zheng.jiwei@connecty.co.jp>
2020-03-25 17:43:47 +0900

    update info.cukes/cucumber-clojure to 1.2.6

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject com.siili/lein-cucumber "1.0.7"
   :description "Run cucumber-jvm specifications with leiningen"
   :url https://github.com/siilisolutions/lein-cucumber
-  :dependencies [[info.cukes/cucumber-clojure "1.2.4"]
+  :dependencies [[info.cukes/cucumber-clojure "1.2.6"]
                  [leiningen-core "2.0.0"]
-                 [org.clojure/clojure "1.6.0"]]
+                 [org.clojure/clojure "1.8.0"]]
   :profiles {:cucumber {:dependencies [[commons-io "2.4"]]
                         :plugins [[com.siili/lein-cucumber "1.0.7"]]}
              :cuke-htmlrep [:cucumber {:cucumber
